2c16d9a934176fc010953ae6f5302e0832082e60
m.ndongonyobe <m.ndongonyobe@cgi.com>
2025-09-02 15:15:27 +0300

    Changing mapping Home Page

--- src/main/java/com/adacorp/task_managementV2/controller/TestController.java
+++ src/main/java/com/adacorp/task_managementV2/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/index")
     public String getSbAdminIndex(Model model){
         model.addAttribute("bonjour","BONJOUR LE MONDE");
         return "index" ;
