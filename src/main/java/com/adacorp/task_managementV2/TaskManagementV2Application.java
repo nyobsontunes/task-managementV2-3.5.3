fa6392c8b798aa7c1625960f44cdacf1c0e771fa
nyobsontunes <nyobsontunes@gmail.com>
2025-09-04 21:39:52 +0100

    SpringBoot Config V3.5.3

--- src/main/java/com/adacorp/task_managementV2/TaskManagementV2Application.java
+++ src/main/java/com/adacorp/task_managementV2/TaskManagementV2Application.java
@@ -2,11 +2,20 @@ package com.adacorp.task_managementV2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan("com.adacorp.task_managementV2.controller")
 @EnableScheduling
-public class TaskManagementV2Application {
+public class TaskManagementV2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TaskManagementV2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementV2Application.class, args);
--- src/main/java/com/adacorp/task_managementV2/controller/TestController.java
+++ src/main/java/com/adacorp/task_managementV2/controller/TestController.java
@@ -3,11 +3,12 @@ package com.adacorp.task_managementV2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
-    @GetMapping(value = "/index")
+    @GetMapping(value = "/welcome-page")
     public String getSbAdminIndex(Model model){
         model.addAttribute("bonjour","BONJOUR LE MONDE");
         return "index" ;
