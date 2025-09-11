70ce19a37572bd62ef91500023c4f2a99c6b2e32
nyobsontunes <nyobsontunes@gmail.com>
2025-09-11 21:07:48 +0100

    SpringBoot USER MANAGEMENT - ADD

--- src/main/java/com/adacorp/task_managementV2/controller/UserController.java
+++ src/main/java/com/adacorp/task_managementV2/controller/UserController.java
@@ -5,20 +5,29 @@ import com.adacorp.task_managementV2.model.Utilisateur;
 import com.adacorp.task_managementV2.services.Impl.UtilisateurServiceImpl;
 import com.adacorp.task_managementV2.services.RoleService;
 import com.adacorp.task_managementV2.services.UtilisateurService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class UserController {
 
+    private static final String USER_SESSION = "connectedUserDB";
     private final UtilisateurService utilisateurService;
     private final RoleService roleService;
-    private static final String USER = "user" ;
+    private static final String UTILISATEUR = "utilisateur" ;
     private static  final String USERS = "users";
     private static final String ROLES =  "roles";
     private static  final String SUCCESS = "success" ;
@@ -29,6 +38,8 @@ public class UserController {
     private static  final String PAGE_USER_DETAIL = "views/user-detail" ;
     private static  final String PAGE_USER_PROFILE = "views/user-profile" ;
     private static  final String USER_ADMIN_ACCOUNT = "admin@admin.com" ;
+    private static  final String MSG_CONFIRMATION_NOT_SAME_PWD = "Confirmation password is not the same as User password" ;
+    private static  final String MSG_NO_SUCH_USER_FOUND = "No such user found" ;
 
     @Autowired
     public UserController(UtilisateurService utilisateurService, RoleService roleService) {
@@ -85,5 +96,118 @@ public class UserController {
         return PAGE_USER_LIST ;
     }
 
+    @GetMapping(value = "/home-add-user")
+    public String addUserGet(Model model, Principal p, HttpServletRequest request){
 
+        List<Role> roleList = this.roleService.findAll() ;
+        Utilisateur utilisateur = new Utilisateur() ;
+        utilisateur.setAccountNonLocked(true);
+
+
+        // ----------------------------------------------------------------------------
+        model.addAttribute(SUCCESS,"Successful Redirection add users page") ;
+        model.addAttribute("utilisateur", utilisateur);
+        setAttributCommun (model);
+        // ----------------------------------------------------------------------------
+
+        return PAGE_USER_ADD ;
+    }
+
+    @PostMapping(value = "/home-add-user")
+    public String addUserPost(@ModelAttribute(UTILISATEUR) Utilisateur utilisateur, Model model , RedirectAttributes redirectAttributes, Principal p, HttpServletRequest request){
+
+        // ----------------------------------------------------------------------------
+        model.addAttribute(SUCCESS,"Successful Redirection add users page") ;
+        model.addAttribute("utilisateur", new Utilisateur());
+        setAttributCommun (model);
+        // ----------------------------------------------------------------------------
+
+
+        List<Role> listRole = this.roleService.findAll() ;
+        model.addAttribute(ROLES,listRole) ;
+        log.info("user.getId() {}", utilisateur.getId());
+
+        if (utilisateur.getConfirmPassword().equals( utilisateur.getPassword() )){
+
+            // String password = this.passwordEncoder.encode   ( utilisateur.getPassword() ) ;
+            String password = utilisateur.getPassword() ;
+            String page = null ;
+            // si le User qui vient du form n'a pas de id alors c'est un nouvel enrégistrement
+            if ( utilisateur.getId() == null ) {
+                utilisateur.setPassword(password);
+                this.utilisateurService.save(utilisateur);
+                model.addAttribute(UTILISATEUR,utilisateur) ;
+                log.info("user.getId()AFTER SAVE "+utilisateur.getId());
+                redirectAttributes.addFlashAttribute(SUCCESS, "Successful Insert Operation...");
+                page = "redirect:/home-list-users" ;
+            }
+            // si le User qui vient du form a un id alors c'est une mise à jour des infos
+            else if (utilisateur.getId() >= 1)
+            {
+                utilisateur.setPassword(password);
+                this.utilisateurService.save(utilisateur);
+                model.addAttribute(UTILISATEUR,utilisateur) ;
+                log.info("user.getId()AFTER UPDATE "+utilisateur.getId());
+                model.addAttribute(SUCCESS, "Successful Update operation...");
+
+                // On modifie les infos d'affichage de l'Utilisateur Connecté
+                /*
+                Optional<Utilisateur>  connectedUserDB = utilisateurService.findByEmail( p.getName() ) ;
+                if ( connectedUserDB.isPresent() && Objects.equals( utilisateur.getId(), connectedUserDB.get().getId()) ){
+                    connectedUserDB.get().setId(utilisateur.getId());
+                    connectedUserDB.get().setPassword(password);
+                    connectedUserDB.get().setLastName(utilisateur.getLastName());
+                    connectedUserDB.get().setFirstName(utilisateur.getFirstName());
+                    connectedUserDB.get().setRole(utilisateur.getRole());
+                    connectedUserDB.get().setEmail(utilisateur.getEmail());
+                    connectedUserDB.get().setTelephone(utilisateur.getTelephone());
+                    connectedUserDB.get().setAccountNonLocked(true);
+                    request.getSession().setAttribute( USER_SESSION, connectedUserDB.get() ) ;
+                }
+                */
+                page = PAGE_USER_ADD ;
+            }
+
+            return page ;
+
+        } else {
+
+            String page = null ;
+
+            if ( utilisateur.getId() == null ){
+                model.addAttribute(SUCCESS,null) ;
+                model.addAttribute(UTILISATEUR,utilisateur) ;
+                redirectAttributes.addFlashAttribute(ERROR,MSG_CONFIRMATION_NOT_SAME_PWD) ;
+                page =  "redirect:/home-add-user" ;
+            } else if (utilisateur.getId() >= 1) {
+                model.addAttribute(SUCCESS,null) ;
+                model.addAttribute(UTILISATEUR,utilisateur) ;
+                model.addAttribute(ERROR,MSG_CONFIRMATION_NOT_SAME_PWD) ;
+                page = PAGE_USER_ADD ;
+            }
+
+            return page ;
+        }
+
+    }
+
+    public void setAttributCommun (Model model){
+        List<Role> roleList = this.roleService.findAll() ;
+        model.addAttribute("Titre", "page.user.addUser") ;
+        model.addAttribute("LabelFistName", "commun.label.firstName") ;
+        model.addAttribute("LabelLastName", "commun.label.lastName") ;
+        model.addAttribute("LabelSexe", "commun.label.sexe") ;
+        model.addAttribute("LabelSexeF", "commun.label.sexeF") ;
+        model.addAttribute("LabelSexeM", "commun.label.sexeM") ;
+        model.addAttribute("LabelMasculin", "commun.label.masculin") ;
+        model.addAttribute("LabelFeminin", "commun.label.feminin") ;
+        model.addAttribute("LabelTelephone", "commun.label.telephone") ;
+        model.addAttribute("LabelEmail", "commun.label.email") ;
+        model.addAttribute("LabelPassword", "commun.label.password") ;
+        model.addAttribute("LabelConfirmPassword", "commun.label.confirmPassword") ;
+        model.addAttribute("LabelRole", "commun.label.role") ;
+        model.addAttribute("LabelSave", "commun.label.save") ;
+        model.addAttribute("LabelHaveAccount", "Have an account? Go to login") ;
+        model.addAttribute("listRole", roleList) ;
+    }
 }
